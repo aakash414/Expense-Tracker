7c51b24013978b994d211ab5f42133a46cfb03be
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-02-06 10:37:04 +0530

    transaction list

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+      <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ transaction.text }} <span>₹{{ transaction.amount }}</span
+        ><button class="delete-btn" @click="deleteTransaction(transaction.id)">
+          x
+        </button>
+      </li>
+    </ul>
+  </template>
+
+    <script>
+    export default {
+        data() {
+            return{
+                transactions: [
+                    {id: 1, text: 'Flower', amount: -20},
+                    {id: 2, text: 'Salary', amount: 300},
+                    {id: 3, text: 'Book', amount: -10},
+                    {id: 4, text: 'Camera', amount: 150}
+                ]
+            }
+        }
+    }
+  </script>
